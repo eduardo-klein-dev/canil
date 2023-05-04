0488767f4bb360252a19cfa05e69f0485c168e76
eduardo-klein-dev <eduardokleindeveloper@gmail.com>
2023-05-04 15:14:09 -0300

    4) Separando e criando as views

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const server = express();
 
 server.set('view engine', 'mustache');
-server.set('viwes', path.join(__dirname, 'views'));
+server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
